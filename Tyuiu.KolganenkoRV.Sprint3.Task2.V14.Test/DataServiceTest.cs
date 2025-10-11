d72b0ef31917603e0ee7ba54cfc0abb6e583fa4f
Tyuiu.KolganenkoRV@outlook.com <kolganenko.rinat@mail.ru>
2025-10-11 18:30:13 +0500

    Task2

--- Tyuiu.KolganenkoRV.Sprint3.Task2.V14.Test/DataServiceTest.cs
+++ Tyuiu.KolganenkoRV.Sprint3.Task2.V14.Test/DataServiceTest.cs
@@ -11,9 +11,9 @@ namespace Tyuiu.KolganenkoRV.Sprint3.Task2.V14.Test
             DataService ds = new DataService();
             int value = 5;
             int startValue = 1;
-            int stopValue = 11;
+            int stopValue = 6;
             double res = ds.GetMultiplySeries(value, startValue, stopValue);
-            double wait = 1.83;
+            double wait = 10220.275;
             Assert.AreEqual(wait, res);
         }
     }
