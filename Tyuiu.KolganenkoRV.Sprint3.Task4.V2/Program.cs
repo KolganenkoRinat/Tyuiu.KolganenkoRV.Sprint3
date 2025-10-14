10ebe75764b5d7adc35c903603bac4607cc69286
Tyuiu.KolganenkoRV@outlook.com <kolganenko.rinat@mail.ru>
2025-10-14 20:40:37 +0500

    Task4

---
+++ Tyuiu.KolganenkoRV.Sprint3.Task4.V2/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.KolganenkoRV.Sprint3.Task4.V2.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #3 | Выполнила : Колганенко Р. В. | ИБКСб-25-1";
+//Длина строки 75 символов
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #3                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #4                                                              *");
+Console.WriteLine("* Вариант #2                                                              *");
+Console.WriteLine("* Выполнил: Колганенко Ринат Витальевич | ИБКСб-25-1                      *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+Console.WriteLine("* функции y=cos(x)/x При х = 0 прервать цикл. Значения суммировать.       *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int startValue = -5;
+int stopValue = 5;
+
+Console.WriteLine("Старт шага = " + startValue);
+Console.WriteLine("Конец шага = " + stopValue);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Сумма ряда = " + ds.Calculate(startValue, stopValue));
+Console.ReadKey();
