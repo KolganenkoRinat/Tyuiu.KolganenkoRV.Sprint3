0f0a9573b0f1bb2765df5e4886c278f6f0990f21
Tyuiu.KolganenkoRV@outlook.com <kolganenko.rinat@mail.ru>
2025-10-23 22:27:48 +0500

    Task5

---
+++ Tyuiu.KolganenkoRV.Sprint3.Task5.V10.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KolganenkoRV.Sprint3.Task5.V10.Lib
+{
+    public class DataService : ISprint3Task5V10
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double SumSeries = 0;
+            int i, j;
+            
+            for (i = startValue1; i < stopValue1; i++)
+            { 
+                for (j = startValue2; j < stopValue2; j++) 
+                {
+                    SumSeries = SumSeries + (Math.Pow(x, 3) * j) + 2;
+                }
+
+            }
+            return Math.Round(SumSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.KolganenkoRV.Sprint3.Task5.V10/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.KolganenkoRV.Sprint3.Task5.V10.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #3 | Выполнила : Колганенко Р. В. | ИБКСб-25-1";
+//Длина строки 75 символов
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #3                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #5                                                              *");
+Console.WriteLine("* Вариант #10                                                              *");
+Console.WriteLine("* Выполнил: Колганенко Ринат Витальевич | ИБКСб-25-1                      *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int x = 5;
+int startValue1 = 1;
+int startValue2 = 1;
+int stopValue1 = 3;
+int stopValue2 = 10;
+
+Console.WriteLine("Старт шага 1 = " + startValue1);
+Console.WriteLine("Старт шага 2 = " + startValue2);
+Console.WriteLine("Конец шага 1 = " + stopValue1);
+Console.WriteLine("Конец шага 2 = " + stopValue2);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Сумма ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+Console.ReadKey();
